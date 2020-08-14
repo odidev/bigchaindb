f48a0b82ac718cb48defe223f199c995c2fe14ca
odidev <odidev@puresoftware.com>
2020-08-14 12:52:34 +0530

    Update travis-before-install.sh

--- .ci/travis-before-install.sh
+++ .ci/travis-before-install.sh
@@ -8,9 +8,9 @@
 if [[ -z ${TOXENV} ]]; then
   sudo apt-get update
   sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
-
-  sudo rm /usr/local/bin/docker-compose
-  curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
-  chmod +x docker-compose
-  sudo mv docker-compose /usr/local/bin
+  sudo pip install docker-compose
+ # sudo rm /usr/local/bin/docker-compose
+ # curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
+ # chmod +x docker-compose
+#  sudo mv docker-compose /usr/local/bin
 fi
